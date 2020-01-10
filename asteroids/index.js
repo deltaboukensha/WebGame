d7d6fb8dd3ff91f572127248c5e5a06fa8d9d782
Daniel Zheng <daniel.zheng@barnebys.com>
2020-01-11 06:30:54 +0800

    refactored

--- asteroids/index.js
+++ asteroids/index.js
@@ -1,8 +1,3 @@
-const canvas = document.getElementById("myCanvas");
-const g = canvas.getContext("2d");
-let oldTime;
-const keyPress = {};
-
 const b2Vec2 = Box2D.Common.Math.b2Vec2;
 const b2BodyDef = Box2D.Dynamics.b2BodyDef;
 const b2Body = Box2D.Dynamics.b2Body;
@@ -14,41 +9,35 @@ const b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
 const b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
 const b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
 
+const canvas = document.getElementById("myCanvas");
+const g = canvas.getContext("2d");
+let oldTime;
+const keyPress = {};
 const world = new b2World(new b2Vec2(0, 0), true);
-
-var fixDef = new b2FixtureDef;
+const bodyDef = new b2BodyDef;
+const fixDef = new b2FixtureDef;
 fixDef.density = 1.0;
 fixDef.friction = 0.5;
 fixDef.restitution = 0.2;
 
-var bodyDef = new b2BodyDef;
-
-//create ground
-bodyDef.type = b2Body.b2_staticBody;
-bodyDef.position.x = 9;
-bodyDef.position.y = 13;
-fixDef.shape = new b2PolygonShape;
-fixDef.shape.SetAsBox(10, 0.5);
-world.CreateBody(bodyDef).CreateFixture(fixDef);
-
 //create some objects
 bodyDef.type = b2Body.b2_dynamicBody;
-// for (var i = 0; i < 10; i++) {
-//     if (Math.random() > 0.5) {
-//         fixDef.shape = new b2PolygonShape;
-//         fixDef.shape.SetAsBox(
-//             Math.random() + 0.1 //half width
-//             , Math.random() + 0.1 //half height
-//         );
-//     } else {
-//         fixDef.shape = new b2CircleShape(
-//             Math.random() + 0.1 //radius
-//         );
-//     }
-//     bodyDef.position.x = Math.random() * 10;
-//     bodyDef.position.y = Math.random() * 10;
-//     world.CreateBody(bodyDef).CreateFixture(fixDef);
-// }
+for (var i = 0; i < 10; i++) {
+    if (Math.random() > 0.5) {
+        fixDef.shape = new b2PolygonShape;
+        fixDef.shape.SetAsBox(
+            Math.random() + 0.1 //half width
+            , Math.random() + 0.1 //half height
+        );
+    } else {
+        fixDef.shape = new b2CircleShape(
+            Math.random() + 0.1 //radius
+        );
+    }
+    bodyDef.position.x = Math.random() * 10;
+    bodyDef.position.y = Math.random() * 10;
+    world.CreateBody(bodyDef).CreateFixture(fixDef);
+}
 
 //create player
 bodyDef.position.x = 10;
@@ -69,6 +58,7 @@ const gameLoop = (newTime) => {
     const deltaTime = newTime - oldTime;
     const playerAngle = player.GetBody().GetAngle();
     const forwardVector = new b2Vec2(Math.cos(playerAngle), Math.sin(playerAngle));
+    const rotationPower = 1.0;
 
     if (keyPress["w"]) {
         const angle = player.GetBody().GetAngle();
@@ -82,11 +72,11 @@ const gameLoop = (newTime) => {
     }
     
     if (keyPress["a"]) {
-        player.GetBody().ApplyTorque(-0.1);
+        player.GetBody().ApplyTorque(-rotationPower);
     }
     
     if (keyPress["d"]) {
-        player.GetBody().ApplyTorque(+0.1);
+        player.GetBody().ApplyTorque(+rotationPower);
     }
 
     world.Step(deltaTime * 0.001, 10, 10);
