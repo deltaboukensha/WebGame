4d4cc8d444a8f9b9e6435075007526b6a1f67379
DeltaZsnes <daniel.zheng@barnebys.com>
2021-01-06 21:14:54 +0100

    add first part of the song

--- skymusic/index.js
+++ skymusic/index.js
@@ -1,14 +1,84 @@
 const instrument = document.getElementById("instrument");
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth();
+// Set the tone to sine
+synth.oscillator.type = "sine";
+// connect it to the master output (your speakers)
+synth.toMaster();
 
+const createButton = (pitchOctave) => {
+    let button = document.createElement("button");
+    instrument.appendChild(button);
+    button.innerText = pitchOctave;
 
-for(let i=0; i<15; i++)
+    button.addEventListener("click", () => {
+        synth.triggerAttackRelease(pitchOctave, "8n");
+    });
+};
+
+createButton("E4");
+createButton("F4");
+createButton("G4");
+createButton("A4");
+createButton("B4");
+createButton("C4");
+createButton("D4");
+createButton("E4");
+createButton("F4");
+createButton("E4");
+createButton("F4");
+createButton("G4");
+createButton("A4");
+createButton("B4");
+createButton("C4");
+
 {
     let button = document.createElement("button");
     instrument.appendChild(button);
+    button.innerText = "Play";
 
     button.addEventListener("click", () => {
-        const now = Tone.now()
-        synth.triggerAttackRelease("C4", "8n", now)
+        let now = Tone.now();
+
+        synth.triggerAttackRelease("A4", "8n", now + 0.0);
+        synth.triggerAttackRelease("B4", "8n", now + 1.0);
+        synth.triggerAttackRelease("C5", "8n", now + 2.0);
+
+        synth.triggerAttackRelease("B4", "8n", now + 3.0);
+        synth.triggerAttackRelease("C5", "8n", now + 4.0);
+        synth.triggerAttackRelease("E5", "8n", now + 5.0);
+        synth.triggerAttackRelease("B4", "8n", now + 6.0);
+
+        synth.triggerAttackRelease("E4", "8n", now + 7.0);
+        synth.triggerAttackRelease("A4", "8n", now + 8.0);
+        
+        synth.triggerAttackRelease("G4", "8n", now + 9.0);
+        synth.triggerAttackRelease("A4", "8n", now + 10.0);
+        synth.triggerAttackRelease("C5", "8n", now + 11.0);
+        synth.triggerAttackRelease("G4", "8n", now + 12.0);
+
+        synth.triggerAttackRelease("F4", "8n", now + 13.0);
+        synth.triggerAttackRelease("E4", "8n", now + 14.0);
+        synth.triggerAttackRelease("F4", "8n", now + 15.0);
+
+        synth.triggerAttackRelease("E4", "8n", now + 16.0);
+        synth.triggerAttackRelease("F4", "8n", now + 17.0);
+        synth.triggerAttackRelease("C5", "8n", now + 18.0);
+        synth.triggerAttackRelease("E4", "8n", now + 19.0);
+
+        {
+            let n = now + 20.0;
+            synth.triggerAttackRelease("E4", "8n", n + 0.0);
+            synth.triggerAttackRelease("E4", "8n", n + 1.0);
+            synth.triggerAttackRelease("E4", "8n", n + 2.0);
+            synth.triggerAttackRelease("D4", "3n", n + 3.0);
+        }
+
+        {
+            let n = now + 24.0;
+            synth.triggerAttackRelease("F4", "8n", n + 1.0);
+            synth.triggerAttackRelease("F4", "8n", n + 2.0);
+            synth.triggerAttackRelease("B4", "8n", n + 3.0);
+            synth.triggerAttackRelease("B4", "8n", n + 4.0);
+        }
     });
 }
